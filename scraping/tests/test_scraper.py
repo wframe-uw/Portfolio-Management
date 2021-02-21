efa6a91b7f2e25dffaf9d3e3f2ee73c7def7c5e9
William Frame <wframe@uw.edu>
2021-02-20 19:51:33 -0800

    more tests

--- scraping/tests/test_scraper.py
+++ scraping/tests/test_scraper.py
@@ -12,7 +12,7 @@ class TestScraper(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestScraper, self).__init__(*args, **kwargs)
         self.data_dir = os.path.join("scraping", "tests", "data")
-
+        self.data_dir = self.data_dir if os.path.isdir(self.data_dir) else os.path.join("data")
     @staticmethod
     def get_test_requester(file_to_read_text_from):
         requester = Requester()
@@ -30,3 +30,10 @@ class TestScraper(unittest.TestCase):
         test_scraper = TestScraper.get_test_scraper(test_requester)
         test_scraper.add_key_stats_to_dict()
         self.assertTrue(test_scraper.financials_dict['grossProfits'] == '96.94B')
+
+    def test_scraper_succesfully_scrapes_valid_profile_url(self):
+        test_data = os.path.join(self.data_dir, VALID_PROFILE_URL_TEXT_FILE_NAME)
+        test_requester = TestScraper.get_test_requester(test_data)
+        test_scraper = TestScraper.get_test_scraper(test_requester)
+        test_scraper.add_profile_to_dict()
+        self.assertTrue(test_scraper.profile_dict['sector'] == 'Technology')
